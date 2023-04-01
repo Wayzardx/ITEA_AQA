b41c0ff4024965b0e2b11f5e62b5fb54e96bc61b
WAYZARD <wayzardz@gmail.com>
2023-04-02 00:05:13 +0300

    home work3 done.

---
+++ src/HomeWork3/Main.java
@@ -0,0 +1,10 @@
+package HomeWork3;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Sum three digits of a number: " + ProcessWithNumbers.showSum(555));
+        System.out.println("Sum length of a two words: " + ProcessWithNumbers.showSum("Hello", "World"));
+        System.out.println("Check if your number is greater than 0: " + ProcessWithNumbers.showSum());
+
+    }
+}
