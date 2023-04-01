cc3d0607425f9c70802fe4e3f3dc386d3cc3ca18
WAYZARD <wayzardz@gmail.com>
2023-04-01 21:47:50 +0300

    try to understand how work GIT

---
+++ src/HomeWork2/Main.java
@@ -0,0 +1,10 @@
+package HomeWork2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char heart = '\u2764';
+        System.out.println("Hello Java WORLD " + heart);
+    }
+}
+test
