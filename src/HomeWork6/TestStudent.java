e0239b063786018035f7cef2a4cc50f7764bbc16
wayzardX <128502593+Wayzardx@users.noreply.github.com>
2023-04-17 04:20:48 +0300

    homework 6 is done. (#4)

---
+++ src/HomeWork6/TestStudent.java
@@ -0,0 +1,15 @@
+package HomeWork6;
+
+public class TestStudent {
+
+    public static void main(String[] args) {
+        Student student1 = new Student("Natasha", 1, 8);
+        student1.showInfo();
+
+        Student student2 = new Student("Ik", 5, 11);
+        student2.showInfo();
+
+        Student student3 = new Student("Tanos", 0, 5);
+        student3.showInfo();
+    }
+}
---
+++ src/HomeWork6_2/Dog.java
@@ -0,0 +1,13 @@
+package HomeWork6_2;
+
+public class Dog extends Pet {
+
+    public Dog(String name) {
+        super();
+        System.out.println("I am dog and my name is: " + name);
+    }
+
+    public void play() {
+        System.out.println("Dog Plays");
+    }
+}
